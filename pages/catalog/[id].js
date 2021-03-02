780322439467061e6cbd711b5555d197c118ef6c
Aleksandr Maliarevskii <luexe@yandex.ru>
2021-03-02 11:19:58 +0300

    add product from detailed

--- pages/catalog/[id].js
+++ pages/catalog/[id].js
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import { useState } from 'react'
+
 import Image from 'next/image'
 
 import MainLayout from '../../layouts/MainLayout'
@@ -6,9 +7,36 @@ import CatalogLayout from '../../layouts/CatalogLayout'
 
 import Button from '../../components/Button'
 
+import {
+    getDataFromLocal,
+    setDataToLocal
+} from '../../storage'
+
 import classnames from './DetailedProduct.module.scss'
 
 const DetailedProduct = () => {
+    const [product, setDetailProduct] = useState({ id: 653, name: ' Масло для тела для предотвращения старения с зеленым чаем и геранью', count: 0, cost: 915 })
+
+    const handleIncrease = () => {
+        setDetailProduct({ ...product, count: product.count + 1 })
+    }
+
+    const handleDecrease = () => {
+        setDetailProduct({ ...product, count: product.count - 1 })
+    }
+
+    const getProductsFromStorage = () => {
+        return getDataFromLocal('phylosophyProducts') || [];
+    }
+
+    const handleSetProduct = () => {
+        const dataFromStorage = getProductsFromStorage()
+        const mappedData = [...dataFromStorage, product]
+        setDataToLocal('phylosophyProducts', mappedData)
+    }
+
+    const { name, cost, count } = product
+
     return (
         <MainLayout>
             <CatalogLayout>
@@ -25,18 +53,18 @@ const DetailedProduct = () => {
                         </div>
                         <div className={classnames['detailed-product__right']}>
                             <h1>
-                                Масло для тела для предотвращения старения с зеленым чаем и геранью
+                                {name}
                             </h1>
                             <span className={classnames['detailed-product__cost']}>
-                                915 руб.
+                                {`${cost}руб.`}
                             </span>
                             <div className={classnames['detailed-product__operations']}>
                                 <div>
-                                    <Button className={classnames['detailed-product__op-btn']} text='-' />
-                                    <span className={classnames['detailed-product__total']}>0</span>
-                                    <Button className={classnames['detailed-product__op-btn']} text='+' />
+                                    <Button className={classnames['detailed-product__op-btn']} onClick={handleDecrease} text='-' disabled={count < 1} />
+                                    <span className={classnames['detailed-product__total']}>{count}</span>
+                                    <Button className={classnames['detailed-product__op-btn']} onClick={handleIncrease} text='+' />
                                 </div>
-                                <Button className={classnames['detailed-product__buy-btn']} text='Купить' />
+                                <Button className={classnames['detailed-product__buy-btn']} onClick={handleSetProduct} text='Купить' />
                             </div>
                             <h2 className={classnames['detailed-product__secondary']}>
                                 Характеристики:
