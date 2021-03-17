f77daa1688631f29dbd91fee8ae5dee519f7ba3c
Aleksandr Maliarevskii <luexe@yandex.ru>
2021-03-17 17:34:42 +0300

    console

--- pages/catalog/category/[id].js
+++ pages/catalog/category/[id].js
@@ -39,7 +39,7 @@ const Category = ({ products, categories, createNotification }) => {
 export async function getServerSideProps({ params }) {
     const products = await WooCommerceApi.get(`products`, { category: params.id }).then(response => response.data).catch(err => err)
     const categories = await WooCommerceApi.get(`products/categories`).then(response => response.data).catch(err => err)
-
+    console.log('safkasjf')
     if (!products) {
         return {
             notFound: true,
