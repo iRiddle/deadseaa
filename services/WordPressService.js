4e8f7c61d243ef7df0fce444ed91578e20ad4819
Aleksandr Maliarevskii <luexe@yandex.ru>
2021-03-18 19:26:47 +0300

    wordpress custom service

--- services/WordPressService.js
+++ services/WordPressService.js
@@ -1,4 +1,30 @@
+import { API_URL } from '../constants'
+
 const WPAPI = require('wpapi');
-const WordPressApi = new WPAPI({ endpoint: 'http://kosmetika.sandev.online/wp-json' });
+const WordPressApi = new WPAPI({ endpoint: API_URL });
+
+export const WordPressCustomApi = (endpoint, method = 'GET', body, headers = {}) => {
+    return fetch(`${API_URL}/wp-json${endpoint}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            ...headers
+        },
+        method,
+        ...(body && { body: JSON.stringify(body) }),
+    })
+        .then((res) => {
+            return res.json();
+        })
+
+        .then((res) => {
+            return {
+                code: res.code,
+                data: res.data,
+                message: res.message
+            }
+
+        })
+        .catch(err => err)
+}
 
 export default WordPressApi
